9f8c94ed47ddcf560cfc64d51abc04392b3552f0
Benjamin Parker <benjaminparker@gmail.com>
2014-11-11 07:58:06 -0600

    background image working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!
   def home
+    redirect_to pins_index_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pins/index'
+  get 'pages/home'
 
   devise_for :users
 
